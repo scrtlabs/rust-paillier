91dc430429d043757645f7688479b696d8c2033f
Assaf Morami <assaf.morami@gmail.com>
2023-03-22 14:39:20 +0200

    Fix serialize missing and argument

--- src/serialize.rs
+++ src/serialize.rs
@@ -5,7 +5,7 @@ pub mod bigint {
     use BigInt;
 
     pub fn serialize<S: ser::Serializer>(x: &BigInt, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_str(&x.to_str_radix(10))
+        serializer.serialize_str(&x.to_str_radix(10, false))
     }
 
     pub fn deserialize<'de, D: de::Deserializer<'de>>(deserializer: D) -> Result<BigInt, D::Error> {
@@ -28,24 +28,28 @@ pub mod bigint {
     }
 }
 
-
 pub mod vecbigint {
 
-    use serde::{de, ser};
-    use serde::ser::SerializeSeq;
     use serde::de::SeqAccess;
+    use serde::ser::SerializeSeq;
+    use serde::{de, ser};
     use std::fmt;
     use BigInt;
 
-    pub fn serialize<S: ser::Serializer>(x: &Vec<BigInt>, serializer: S) -> Result<S::Ok, S::Error> {
+    pub fn serialize<S: ser::Serializer>(
+        x: &Vec<BigInt>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error> {
         let mut seq = serializer.serialize_seq(Some(x.len()))?;
         for e in x {
-            seq.serialize_element(&e.to_str_radix(10))?;
+            seq.serialize_element(&e.to_str_radix(10, false))?;
         }
         seq.end()
     }
 
-    pub fn deserialize<'de, D: de::Deserializer<'de>>(deserializer: D) -> Result<Vec<BigInt>, D::Error> {
+    pub fn deserialize<'de, D: de::Deserializer<'de>>(
+        deserializer: D,
+    ) -> Result<Vec<BigInt>, D::Error> {
         struct VecBigIntVisitor;
 
         impl<'de> de::Visitor<'de> for VecBigIntVisitor {
@@ -56,7 +60,9 @@ pub mod vecbigint {
             }
 
             fn visit_seq<A>(self, mut seq: A) -> Result<Vec<BigInt>, A::Error>
-                where A: SeqAccess<'de> {
+            where
+                A: SeqAccess<'de>,
+            {
                 let mut values: Vec<BigInt> = Vec::new();
                 while let Some(value) = seq.next_element::<String>()? {
                     values.push(BigInt::from_str_radix(&value, 10).unwrap());
